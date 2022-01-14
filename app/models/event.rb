8c1163f0676ae629b4024a55b3ad3a5c11efdd44
snehithlal <pssnehithlal@gmail.com>
2022-01-14 14:35:24 +0530

    :zap: rails migration changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,12 +8,11 @@ class Event < CircleScopedRecord
 
   before_create :add_host_to_the_user_events
 
-  validate :validate_end_time_after_start_time, if: proc { |event| event.end_time.present? }
-
   validates :title, presence: true
   validates :description, presence: true
   validates :host_id, presence: true
   validates :start_time, presence: true
+  validates_comparison_of :end_time, greater_than: :start_time, if: proc { |event| event.end_time.present? }
 
   # FIXME: check uniqueness with scope in nested params
   accepts_nested_attributes_for :user_events, allow_destroy: true
@@ -25,10 +24,6 @@ class Event < CircleScopedRecord
                                         .order('priority ASC NULLS LAST, start_time ASC') }
 
   private
-
-  def validate_end_time_after_start_time
-    errors.add(:end_time, 'cant be before start time') if end_time <= start_time
-  end
 
   def add_host_to_the_user_events
     user_events.build(user_id: host_id, event_role: 'admin')
