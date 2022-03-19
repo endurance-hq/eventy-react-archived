a80395159dd2aebbaf961890d57c05ed053ebaf7
snehithlal <pssnehithlal@gmail.com>
2022-03-19 15:43:07 +0530

    Modified coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,7 @@
 require 'simplecov'
 
 SimpleCov.start do
-  add_filter '/spec/'
+  add_filter 'spec/'
   add_group 'Models', 'app/models'
   add_group 'Mailers', 'app/mailers'
   add_group 'Controllers', 'app/controllers'
