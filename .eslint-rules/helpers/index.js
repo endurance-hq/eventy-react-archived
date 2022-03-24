da6babf06aeb062a23e9b5edbbd1c155795cb680
Ahammed Nafih BK <ahammednafihbk@gmail.com>
2022-03-24 23:34:23 +0530

    Frontend setup - Linters and Context (#21)
    * Linter and pre commit hook setup
    
    * Auth and user context setup
    
    * Added erblinter
    
    * Updated github actions to include linters
    
    * Added erblint config

---
+++ .eslint-rules/helpers/index.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+
+const buildPathGroupsBasedOnEsbuildAliases = ({
+  customJSRoot = "app/javascript/",
+  customAliasPath = "config/esbuild/alias.js",
+}) => {
+  const rootOfProject = __dirname + `/../../`;
+
+  const isFile = filePath =>
+    fs.existsSync(filePath) && fs.lstatSync(filePath).isFile();
+
+  const esbuildAliasPath = rootOfProject + customAliasPath;
+
+  const hasEsbuildAliasConfig = isFile(esbuildAliasPath);
+
+  const isRailsProject = isFile(rootOfProject + "Gemfile");
+
+  const emptyPathGroups = [];
+
+  if (!hasEsbuildAliasConfig || !isRailsProject) return emptyPathGroups;
+
+  const aliases = require(esbuildAliasPath);
+
+  const railsJSFilesRoot = rootOfProject + customJSRoot;
+
+  const pathGroups = Object.entries(aliases).map(([name, path]) => {
+    // sometimes alias might be already resolved to full absolute path
+    const isAleadyAnAbsolutePath = path.includes("app/");
+
+    const absolutePath = isAleadyAnAbsolutePath
+      ? path
+      : `${railsJSFilesRoot}${path}`;
+    const wildCard =
+      isFile(absolutePath + ".js") || isFile(absolutePath + ".jsx")
+        ? ""
+        : "/**";
+
+    let group = "internal";
+
+    return { pattern: `${name}${wildCard}`, group };
+  });
+
+  return pathGroups;
+};
+
+module.exports = { buildPathGroupsBasedOnEsbuildAliases };
