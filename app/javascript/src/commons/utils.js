b4b7bf5c8250b2ab25b5cec321a8087884a0b807
Ahammed Nafih <ahammednafihbk@gmail.com>
2022-05-15 15:13:45 +0530

    Added ramda

--- app/javascript/src/commons/utils.js
+++ app/javascript/src/commons/utils.js
@@ -1,4 +1,5 @@
 import { createStandaloneToast } from "@chakra-ui/react";
+import { isEmpty } from "ramda";
 
 export const getFromLocalStorage = key => JSON.parse(localStorage.getItem(key));
 
@@ -10,21 +11,6 @@ export const showToast = (status, message) => {
     status,
     isClosable: true,
   });
-};
-
-export const isEmpty = obj => {
-  // eslint-disable-next-line eqeqeq
-  if (obj == null) return true;
-
-  if (typeof obj === "number") return false;
-
-  if (typeof obj === "string" || obj instanceof String) return obj === "";
-
-  if (Array.isArray(obj)) return obj?.length === 0 || false;
-
-  if (typeof obj === "object") return Object.keys(obj).length === 0;
-
-  return !!obj;
 };
 
 export const isPresent = obj => !isEmpty(obj);
